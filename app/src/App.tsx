354198c66f02f3ba2987fe82114f647e2e602e3e
Guilherme Lopes <Gabulhas@users.noreply.github.com>
2022-11-25 17:50:21 +0000

    Not working :(

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,19 +1,19 @@
-import ReactXnft, { View, Text } from "react-xnft";
-//import {GetLandingPageScreen} from "./LandingPage"
-//import { black } from "./Theme/colors";
+import ReactXnft, { View } from "react-xnft";
+import {GetLandingPageScreen} from "./LandingPage"
+import { black } from "./Theme/colors";
 
 //
 // On connection to the host environment, warm the cache.
 //
 ReactXnft.events.on("connect", () => {
   
-  // no-op  <GetLandingPageScreen/>
+  // no-op
 });
 
 export function App() {
   return (
-    <View style={{ height: "100%"}}>
-        <Text>Hey </Text>
+    <View style={{ height: "100%", backgroundColor: black}}>
+        <GetLandingPageScreen/>
     </View>
   );
 }
